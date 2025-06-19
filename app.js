d7d41110abb7a77c9dc536b9fd891c0fff296120
chaitanya <chaitanya@gmail.com>
2025-06-19 19:16:54 +0530

    second-commit

--- app.js
+++ app.js
@@ -9,10 +9,8 @@ app.use(cors());
 app.use(express.json());
 app.use('/tasks', taskRoutes);
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
   console.log('MongoDB Connected');
   app.listen(process.env.PORT || 5000, () => console.log(`Server running on http://localhost:${process.env.PORT || 5000}`));
 }).catch((err) => console.error(err));
