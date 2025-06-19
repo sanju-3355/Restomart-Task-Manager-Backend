6e9d8d15ad0c294b9156395a627a165150aa839e
chaitanya <chaitanya@gmail.com>
2025-06-19 19:01:47 +0530

    first-commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+const taskRoutes = require('./routes/taskRoutes');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use('/tasks', taskRoutes);
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('MongoDB Connected');
+  app.listen(process.env.PORT || 5000, () => console.log(`Server running on http://localhost:${process.env.PORT || 5000}`));
+}).catch((err) => console.error(err));
